779ea869cdc47f86fba6d4acee9e9b9c7d13e4c8
The Test Coder <thetestcoder@gmail.com>
2020-08-17 10:49:26 +0530

    intial

--- routes/writer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Helpers\Facades\UserFacade;
-
-Route::group(['prefix' => 'writer', 'middleware' => ['auth', 'role:writer']], function () {
-    Route::get('/home', 'HomeController@index')->name('home');
-});
